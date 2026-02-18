108a6aea64ea923ec639dc73bb1cf1f4a77fb2c9
kaifmohammed009 <smohamedkaif6@gmail.com>
2026-02-18 09:46:26 +0530

    Fix CORS to allow Netlify frontend

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,20 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Expense = require("../models/Expense");
-// GET all expenses
-router.get("/", async (req, res) => {
-const expenses = await Expense.find().sort({ date: -1 });
-res.json(expenses);
+
+router.get('/', (req, res) => {
+  res.send("Expense route working");
 });
-// ADD expense
-router.post("/", async (req, res) => {
-const expense = new Expense(req.body);
-const saved = await expense.save();
-res.json(saved);
-});
-// DELETE expense
-router.delete("/:id", async (req, res) => {
-await Expense.findByIdAndDelete(req.params.id);
-res.json({ message: "Expense deleted" });
-});
+
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const app = express();
 connectDB();
-app.use(cors());
+app.use(cors({ origin: "https://lucent-mousse-023fec.netlify.app/" cd}));
 app.use(express.json());
-app.use("/api/expenses", require("./routes/expenseRoutes"));
-app.listen(5000, () => console.log("Server running on port 5000"));
+const expenseRoutes = require("./routes/expenseRoutes");
+app.use("/api/expenses", expenseRoutes);
+const PORT = process.env.PORT || 5000;
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
