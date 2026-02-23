03ea3a141b45b5967003b48fe37e7a84716a4923
kaifmohammed009 <smohamedkaif6@gmail.com>
2026-02-23 23:21:23 +0530

    Final CORS fix

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+
 const app = express();
 connectDB();
-app.use(cors({ origin: true, credentials: true }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  }),
+);
+
 app.use(express.json());
+
 const expenseRoutes = require("./routes/expenseRoutes");
 app.use("/api/expenses", expenseRoutes);
+
 const PORT = process.env.PORT || 5000;
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+app.listen(PORT, () => console.log("Server running"));
